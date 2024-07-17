595847aa3f2ebb82e3d6a18fae3eab4dfe6780d6
Richee Njuguna <108935909+richeenjugush@users.noreply.github.com>
2024-07-17 22:41:56 +0300

    chngd images and added script

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const notFoundElement = document.getElementById("not-found");
+  if (notFoundElement) {
+    notFoundElement.classList.add("active");
+  }
+});
